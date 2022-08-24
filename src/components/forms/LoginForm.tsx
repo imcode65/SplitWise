9d9fbf5799fa2e0eaddab9d413d4c83927c0c92d
kanbei0605 <kanbei0605@gmail.com>
2022-08-24 22:44:38 +0900

    updated login form

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -3,22 +3,28 @@ const LoginForm = () => {
     <div className="lg:w-1/2 xl:w-1/2 px-4 md:px-16 shadow-lg">
       <div className="text-3xl font-mont mb-4">Log in</div>
       <form action="/authentication" accept-charset="UTF-8" method="post">
-        <label>Email address</label>
-        <div className="mb-4 w-full">
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Email address
+          </label>
           <input
             type="email"
-            className="w-full"
-            name="credentials[identity]"
-            id="credentials_identity"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="john.doe@company.com"
+            required
           />
         </div>
-        <label>Password</label>
-        <div className="mb-4 w-full">
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Password
+          </label>
           <input
             type="password"
-            className="w-full"
-            name="credentials[password]"
-            id="credentials_password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="•••••••••"
+            required
           />
         </div>
         <div>
