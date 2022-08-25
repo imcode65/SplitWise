bdead501b21d96d656d82df45ee1bfc910adeab1
kanbei0605 <kanbei0605@gmail.com>
2022-08-26 06:26:34 +0900

    removed border-top

--- src/components/layouts/bars/ActivityBar.tsx
+++ src/components/layouts/bars/ActivityBar.tsx
@@ -1,7 +1,7 @@
 const ActivityBar = () => {
   return (
     <div>
-      <div className="bg-[#eee] flex py-2 px-4 border-t-1 border-b-1 border-gray-600 flex-wrap">
+      <div className="bg-[#eee] flex py-2 px-4 border-b-1 border-gray-600 flex-wrap">
         <span className="md:text-3xl sm:text-xl font-semibold">Recent activity</span>
       </div>
       <div className="px-4">
