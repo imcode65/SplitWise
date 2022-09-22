ce6c789a317a191123e3ee9a8fcf5d60d79ac362
kanbei0605 <kanbei0605@gmail.com>
2022-09-23 05:07:24 +0900

    updated server url

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const API_SERVER_URL = 'http://localhost:5000/';
+// export const API_SERVER_URL = 'http://localhost:5000/';
+export const API_SERVER_URL = 'https://splitwise-back.herokuapp.com/';
